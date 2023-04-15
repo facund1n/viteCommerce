8fb8960528f044210eebe9618470f59536ef1514
facund1n <facus16s@gmail.com>
2023-04-15 01:23:02 -0300

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <>
+      <div className="text-center bg-warning py-4">
+        <h1>Welcome</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          perferendis quia atque voluptas fugit veniam eligendi ipsa, animi
+          architecto sit unde consequatur voluptatibus omnis, distinctio
+          molestiae. Impedit veniam architecto quaerat?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;

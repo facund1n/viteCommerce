634a30119e7489a3281e70f71e6592c89cd05314
facund1n <facus16s@gmail.com>
2023-04-15 01:22:56 -0300

    navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+function BrandExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="15590524.webp"
+              src="/15590524.webp"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Vite Commerce
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default BrandExample;

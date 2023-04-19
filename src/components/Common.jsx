e24f9f7b6f01a6ab7662ec2c449752b83400ab2c
facund1n <facus16s@gmail.com>
2023-04-19 01:44:48 -0300

    Styled component ++

---
+++ src/components/Common.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const FormSuccess = styled.span`
+  color: #8ffc00;
+  font-size: 30px;
+  min-height: 100%;
+  font-weight: 600;
+`;
+
+export const FormError = styled.span`
+  color: #f1250f;
+  font-size: 30px;
+  min-height: 20px;
+  font-weight: 600;
+`;
+
+export const SavedPostSuccess = styled.span`
+  color: #8ffc00;
+  font-size: 16px;
+  min-height: 100%;
+  font-weight: bolder;
+`;
+
+export const SavedPostError = styled.span`
+  color: #ffffff;
+  font-size: 24px;
+  min-height: 100%;
+  font-weight: 500;
+`;
+
+export const LikeSuccess = styled.span`
+  color: #8ffc00;
+  font-size: 16px;
+  min-height: 100%;
+  font-weight: bolder;
+`;
+
+export const LikeError = styled.span`
+  color: #ffffff;
+  font-size: 16px;
+  min-height: 100%;
+  font-weight: 500;
+`;
+
+export const Success = styled.span`
+  color: #8ffc00;
+  font-size: 20px;
+  min-height: 100%;
+  font-weight: 600;
+`;
+
+export const Error = styled.span`
+  color: #f1700f;
+  font-size: 20px;
+  min-height: 20px;
+  font-weight: 600;
+`;

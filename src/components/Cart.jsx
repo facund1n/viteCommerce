e62eeb346485651a259744455fcc02e1d058853e
facund1n <facus16s@gmail.com>
2023-04-26 02:41:42 -0300

    cart detail visuals update + total to pay function

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,12 +3,13 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
 
-const Cart = ({ cart, deleteOne, clearCart }) => {
+const Cart = ({ cart, deleteOne, clearCart, totalP }) => {
   return (
     <Container>
-      <Row lg="3" md="3" xs="1">
+      <Row>
         {cart.length === 0 || null || undefined ? (
           <p>
             Cart is empty,{" "}
@@ -18,43 +19,50 @@ const Cart = ({ cart, deleteOne, clearCart }) => {
           </p>
         ) : (
           cart.map((element, key) => (
-            <>
-              <Col key={key}>
-                <Card
-                  className="p-1 d-flex flex-column align-items-center justify-content-between"
-                  style={{ height: "200px" }}
-                >
-                  <Card.Text>
-                    <strong>{element.title}</strong>
-                  </Card.Text>
-                  <Card.Text>
-                    Price <strong>$ {element.price}</strong>
-                  </Card.Text>
-                  <Card.Text>
-                    Quantity: <strong>{element.q}</strong>
-                  </Card.Text>{" "}
-                  <Button
-                    onClick={() => deleteOne(element.id)}
-                    variant="danger"
-                  >
-                    DELETE
-                  </Button>
-                </Card>
-              </Col>
-              <Col>
-                <Button
-                  /* VER SI SE HACE MODAL PARA CONFIRMAR */
-                  className="d-block ms-auto my-1"
-                  onClick={() => clearCart()}
-                  variant="outline-danger"
-                >
-                  CLEAR CART
-                </Button>
-              </Col>
-            </>
+            <Container className="my-1" key={key}>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Row>
+                    <Col xs="8">
+                      <strong>{element.title}</strong>
+                      <br />
+                      <span>
+                        Price: <strong>$ {element.price}</strong>
+                      </span>
+                      <br />
+                      <span>
+                        Quantity: <strong>{element.q}</strong>
+                      </span>
+                    </Col>
+                    <Col xs="4">
+                      <Button
+                        className="d-block ms-auto"
+                        onClick={() => deleteOne(element.id)}
+                        variant="danger"
+                      >
+                        DELETE
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Container>
           ))
         )}
       </Row>
+      <Button
+        /* VER SI SE HACE MODAL PARA CONFIRMAR */
+        className="d-block ms-auto my-1"
+        onClick={() => clearCart()}
+        variant="outline-danger"
+      >
+        CLEAR CART
+      </Button>
+      <ListGroup>
+        <ListGroup.Item>
+          TOTAL: ${cart == [] ? <span>0</span> : totalP(cart)}
+        </ListGroup.Item>
+      </ListGroup>
     </Container>
   );
 };

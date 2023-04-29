9b6aa3685a82d06b630f8f77de16b58264a4b0ea
facund1n <facus16s@gmail.com>
2023-04-29 05:04:53 -0300

    Producto panel visual update

--- src/components/crud/PanelProducts.jsx
+++ src/components/crud/PanelProducts.jsx
@@ -5,32 +5,35 @@ import { Link } from "react-router-dom";
 
 const PanelProducts = ({ data }) => {
   return (
-    <ListGroup>
-      <ListGroup.Item className="bg-white">
-        <Row xs={1} md={1} lg={6}>
-          <Col>
-            <img
-              style={{ height: "120px" }}
-              className="img-fluid"
-              src={data.image[0]}
-              alt={data.image[0]}
-            />
-            <b>ID: {data._id} </b>
-            <h5>{data.title}</h5>
-            <span className="bg-danger text-white p-1">{data.category}</span>
-          </Col>
-          <Col>
-            <Link to={`/products/${data._id}`}>VIEW PRODUCT</Link>
-          </Col>
-          {/*           <Col className="text-end">
-            <EditPostButton data={data} />
-          </Col>
-          <Col className="text-end">
-            <DeletePostButton data={data} />
-          </Col> */}
-        </Row>
-      </ListGroup.Item>
-    </ListGroup>
+    <>
+      <ListGroup>
+        <ListGroup.Item className="bg-white">
+          <Row xs={1} md={1} lg={6}>
+            <Col>
+              <img
+                style={{ height: "120px" }}
+                className="img-fluid"
+                src={data.image[0]}
+                alt={data.image[0]}
+              />
+              <b>ID: {data._id} </b>
+              <h5>{data.title}</h5>
+              <span className="bg-danger text-white p-1">{data.category}</span>
+            </Col>
+            <Col>
+              <Link to={`/products/${data._id}`}>VIEW PRODUCT</Link>
+            </Col>
+            <Col className="text-end">
+              edit{/* <EditPostButton data={data} /> */}
+            </Col>
+            <Col className="text-end">
+              delete
+              {/*    <DeletePostButton data={data} /> */}
+            </Col>
+          </Row>
+        </ListGroup.Item>
+      </ListGroup>
+    </>
   );
 };
 

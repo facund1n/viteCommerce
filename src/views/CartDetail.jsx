65b420455f26b8d3834703435c06400cd2e89113
facund1n <facus16s@gmail.com>
2023-04-30 05:20:43 -0300

    min-vh-100 to fit footer

--- src/views/CartDetail.jsx
+++ src/views/CartDetail.jsx
@@ -1,20 +1,25 @@
 import BuyerForm from "../components/BuyerForm";
 import NavBar from "../components/NavBar";
 import Cart from "../components/Cart";
+import Footer from "../components/Footer";
+import { Container } from "react-bootstrap";
 
 const CartDetail = ({ cart, deleteOne, clearCart, totalP }) => {
   return (
-    <div>
+    <>
       {/* ver si se pasa prop cart o data */}
       <NavBar />
-      <Cart
-        cart={cart}
-        deleteOne={deleteOne}
-        clearCart={clearCart}
-        totalP={totalP}
-      />
-      <BuyerForm />
-    </div>
+      <Container className="min-vh-100">
+        <Cart
+          cart={cart}
+          deleteOne={deleteOne}
+          clearCart={clearCart}
+          totalP={totalP}
+        />
+        <BuyerForm />
+      </Container>
+      <Footer />
+    </>
   );
 };
 

c2bb39b385d68482c6d33fd8dd9b6ffcac7d66fa
facund1n <facus16s@gmail.com>
2023-04-30 05:19:57 -0300

    footer add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark p-5">
+      <p className="text-white text-center">FOOTER</p>
+    </footer>
+  );
+};
+
+export default Footer;

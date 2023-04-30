3d9ff5c2a3e1ec191169b9ce8e49e0bce0b566c9
facund1n <facus16s@gmail.com>
2023-04-29 23:31:17 -0300

    visuals del

--- src/components/crud/AllProductsPanel.jsx
+++ src/components/crud/AllProductsPanel.jsx
@@ -1,18 +1,19 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import PanelProducts from "./PanelProducts";
 import NewProductButton from "./NewProductButton";
+import Container from "react-bootstrap/Container";
 //3.2
 
 const AllProductsPanel = ({ data }) => {
   return (
-    <>
+    <Container>
       <NewProductButton />
       <ListGroup>
         {data.map((data) => (
           <PanelProducts data={data} key={data._id} />
         ))}
       </ListGroup>
-    </>
+    </Container>
   );
 };
 

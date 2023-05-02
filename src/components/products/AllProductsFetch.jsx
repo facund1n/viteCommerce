b6ade9c8bc1d995179b5d7af02fc7460c3f381bb
facund1n <facus16s@gmail.com>
2023-05-02 20:05:46 -0300

    loader add

--- src/components/products/AllProductsFetch.jsx
+++ src/components/products/AllProductsFetch.jsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
 import AllProductsGrid from "./AllProductsGrid";
 import Cart from "../Cart";
+import Loader from "../Loader";
 
 const AllProductsFetch = ({ addToCart }) => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://vite-commerce-back-end.vercel.app/products")
       .then((res) => res.json())
-      .then((json) => setData(json));
+      .then((json) => setData(json))
+      .then(setIsLoading(false));
   }, []);
 
   return (
     <>
-      <AllProductsGrid data={data} addToCart={addToCart} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <AllProductsGrid data={data} addToCart={addToCart} />
+      )}
     </>
   );
 };

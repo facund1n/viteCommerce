065f6909939437de589a72cc56bcc8c2a9a7ab10
facund1n <facus16s@gmail.com>
2023-05-04 01:20:49 -0300

    visuals update + conditional rendering

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -86,6 +86,7 @@ const Routing = ({
               totalP={totalP}
               auth={auth}
               userLogged={userLogged}
+              isAdmin={isAdmin}
             />
           }
         />

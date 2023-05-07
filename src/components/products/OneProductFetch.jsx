045002447754f0e90adb991d11296dd3e888a23a
facund1n <facus16s@gmail.com>
2023-05-07 04:47:59 -0300

    viosuals + prop pass

--- src/components/products/OneProductFetch.jsx
+++ src/components/products/OneProductFetch.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import OneProductDetail from "./OneProductDetail";
 import Loader from "../Loader";
 
-const OneProductFetch = ({ addToCart, cart, auth }) => {
+const OneProductFetch = ({ addToCart, cart, auth, isAdmin }) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
@@ -25,6 +25,7 @@ const OneProductFetch = ({ addToCart, cart, auth }) => {
           addToCart={addToCart}
           cart={cart}
           auth={auth}
+          isAdmin={isAdmin}
         />
       )}
     </>

d9382046af9433cc63059319a29a48f47a63803e
facund1n <facus16s@gmail.com>
2023-05-10 21:45:35 -0300

    about component add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+import Container from "react-bootstrap/Container";
+import Accordion from "react-bootstrap/Accordion";
+
 const Header = () => {
   return (
     <>
@@ -7,6 +10,29 @@ const Header = () => {
           Fully Frontend + Backend integration - fake commerce using vite's
           build (work in progress)
         </p>
+        <Container>
+          <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>About this proyect:</Accordion.Header>
+              <Accordion.Body>
+                <ul className="d-flex flex-column align-items-start text-right">
+                  <span className="fw-bold">Front end:</span>
+                  <li>React JS | Bootstrap</li>
+                  <span className="fw-bold">Back end:</span>
+                  <li>Express | NodeJS | Mongoose </li>
+                  <span className="fw-bold">Dependencies:</span>
+                  <li>
+                    Axios | Formik & Yup | JS-Cookie | React-auth-kit |
+                    React-toastify | React-slick/carousel | Styled-components
+                  </li>
+                  <li>BcryptJS | Cors | dotenv | Jsonwebtoken | Nodemon</li>
+                  <span className="fw-bold">Others:</span>
+                  <li>Postman</li>
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Container>
       </header>
     </>
   );
